c44fda3f9458ae1cb309a54993305bc65ff6aa63
NguyenLedangKhoa <62926705+NguyenLedangKhoa@users.noreply.github.com>
2021-12-14 09:09:08 +0700

    Delete bangame directory

--- bangame/chucnang/timkiem/timkiem.php
+++
@@ -1,4 +0,0 @@
-<form method="post" name="sform" action="index.php?page_layout=danhsachtimkiem">
-    <input type="submit" name="sbutton" value="" />
-    <input type="text" name="stext" placeholder="Tìm kiếm sản phẩm"/>
-</form>
