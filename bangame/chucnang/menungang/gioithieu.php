024a7edb6f61377fd8ed7ed82455227e68ef1e2e
NguyenLedangKhoa <62926705+NguyenLedangKhoa@users.noreply.github.com>
2021-12-14 09:05:16 +0700

    Add files via upload

---
+++ bangame/chucnang/menungang/gioithieu.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" type="text/css" href="css/gioithieu.css" />
+
+<div class="prd-block">
+	<h2>Giới thiệu về K3mart</h2>
+    <div class="about">
+    	<p>Shop bán game PS4</p>
+        <p><span>Địa chỉ</span><br />Đà Lạt - Lâm Đồng</p>	
+			<p><span>Sinh viên thực hiện</span><br />• 1812781_Nguyễn Lê Đăng Khoa<br />• 1812726_ Nguyễn Trần Việt Anh<br />• 1812728_Bùi Quốc Bảo</p>
+        <p id="signature"><span>K3mart Shop Game PS4</span></p>
+    </div>
+</div>
---
+++ bangame/chucnang/quangcao/quangcao.php
@@ -0,0 +1,5 @@
+<div class="l-sidebar">
+	<h2>Game hot</h2>
+	<div class="l-banner"><a href="#"><img width="216" src="anh/banner02.png" /></a>
+	</div>
+</div>
